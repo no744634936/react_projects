43cf997e7af8c6d1b23f85486c416f42b5f42055
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-14 00:25:14 +0900

    weather_app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import './App.css';
 
-export default function App() {
-    return (
-        <div>
-            <h1>hahaha</h1>
-        </div>
-    )
+import React, { useState } from 'react';
+const api = {
+  key: "ed7eae10bb8d040edcdbdf705eca04c0",
+  base: "http://api.openweathermap.org/data/2.5/"
 }
 
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+        //把result的值给weather
+        setWeather(result);
+        ////当按下enter之后，search栏变空 ,为什么会是分号
+        setQuery("");
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)} //???
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {/* 如果没有开始取api的值就返回空字符串 */}
+        {(typeof weather.main != "undefined") ? (
+        //条件句里有很多div时必须用一个div把他们圈起来。
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/weather_api.js
@@ -0,0 +1,4 @@
+export const api={
+    key:"ed7eae10bb8d040edcdbdf705eca04c0",
+    base:"http://api.openweathermap.org/data/2.5/",
+}
